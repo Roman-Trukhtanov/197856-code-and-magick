ce3c24d3ce79b9f692fb88d95b96b89cf2329acd
Roman Trukhtanov <romantrukhtanov@gmail.com>
2018-06-05 18:40:44 +0500

    Поправляет пакет замечаний №2

--- js/stat.js
+++ js/stat.js
@@ -10,20 +10,19 @@ var CLOUD_COLOR = '#ffffff';
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var POSITION_X = 100;
-var POSITION_Y = 10;
+var CLOUD_POSITION_X = 100;
+var CLOUD_POSITION_Y = 10;
 
 var GAP = 10;
 var OFFSET = 5;
 var PADDING_TOP = 15;
 
 var BAR_GAP = 50;
-
 var BAR_WIDTH = 40;
 var MAX_BAR_HEIGHT = 150;
 var CURRENT_PLAYER_BAR_COLOR = '#ff0000';
 
-var startPositionX = POSITION_X + BAR_WIDTH;
+var startPositionX = CLOUD_POSITION_X + BAR_WIDTH;
 var startPositionY = CLOUD_HEIGHT - OFFSET;
 
 /* функция для отрисовки облака */
@@ -46,57 +45,67 @@ var getMaxElement = function (arr) {
 };
 
 /* Функция для сортировки времени мрохождения всех игроков (кроме текущего, т.е игрока с именем "Вы") */
-var sortingPlayersTimes = function (playersNames, playersTimes) {
-  /* Объявляет пустой массив, который будет хранить время прохождения игры всех игроков (кроме текущего, т.е игрока с именем "Вы") */
-  var playersTimesDataBase = [];
+var sortPlayersTimes = function (playersNames, times) {
+  /* Записывает отфильтрованный масив в переменную */
+  var playersTimes = filterPlayersTimes('Вы', playersNames, times);
 
-  /* Добавляет данные в новый массив */
-  for (var k = 0; k < playersTimes.length; k++) {
-    if (playersNames[k] !== 'Вы') {
-      playersTimesDataBase.push(playersTimes[k]);
+  /* Сортирует отфильтрованный массив */
+  sortAscending(playersTimes);
+
+  return playersTimes;
+};
+
+/* Возвращает массив игроков (за исключеним игрока с определенным именем) */
+var filterPlayersTimes = function (name, playersNames, times) {
+  var players = [];
+
+  for (var k = 0; k < times.length; k++) {
+    if (playersNames[k] !== name) {
+      players.push(times[k]);
     }
   }
 
-  /* Сортирует новый массив с временем каждого игрока (по возрастанию) */
-  for (var i = 0; i < playersTimesDataBase.length - 1; i++) {
-    var maxElement = playersTimesDataBase[i];
+  return players;
+};
 
-    for (var j = i + 1; j < playersTimesDataBase.length; j++) {
-      if (playersTimesDataBase[j] > maxElement) {
-        maxElement = playersTimesDataBase[j];
-        var swap = playersTimesDataBase[i];
-        playersTimesDataBase[i] = maxElement;
-        playersTimesDataBase[j] = swap;
+/* Функция сортирующая элементы массива (по возрастанию) */
+var sortAscending = function (arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    var maxElement = arr[i];
+
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] > maxElement) {
+        var swap = arr[i];
+        maxElement = arr[j];
+        arr[i] = maxElement;
+        arr[j] = swap;
       }
     }
   }
-
-  return playersTimesDataBase;
 };
 
 window.renderStatistics = function (ctx, names, times) {
   /* Рисует подложку от статистики */
-  renderCloud(ctx, POSITION_X + GAP, POSITION_Y + GAP, CLOUD_SHADOW_COLOR);
-  renderCloud(ctx, POSITION_X, POSITION_Y, CLOUD_COLOR);
+  renderCloud(ctx, CLOUD_POSITION_X + GAP, CLOUD_POSITION_Y + GAP, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_POSITION_X, CLOUD_POSITION_Y, CLOUD_COLOR);
 
   /* Задает размеры текста */
   ctx.textBaseline = 'top';
   ctx.fillStyle = TEXT_COLOR;
   ctx.font = TEXT_SIZE.toString() + 'px' + ' ' + TEXT_FONT_FAMILY;
 
-  /* Выводит победный текст */
-  ctx.fillText('Ура вы победили!', POSITION_X + GAP + OFFSET, POSITION_Y + PADDING_TOP + OFFSET);
-  ctx.fillText('Список результатов:', POSITION_X + GAP + OFFSET, POSITION_Y + PADDING_TOP + OFFSET + TEXT_HEIGHT);
-
   /* Изменяет базовую линию текста, для удобства подсчетов*/
   ctx.textBaseline = 'bottom';
 
   if (!times) {
-    ctx.font = TEXT_SIZE.toString() + 'px' + ' ' + 'Times New Roman';
     ctx.fillStyle = TEXT_ERROR_COLOR;
-    ctx.fillText('Что-то пошло не так!!!', POSITION_X + GAP + OFFSET, CLOUD_HEIGHT / 2);
-    ctx.fillText('Попробуйте пройти игру заного', POSITION_X + GAP + OFFSET, CLOUD_HEIGHT / 2 + TEXT_HEIGHT);
+    ctx.fillText('Ура, поздравляем!', CLOUD_POSITION_X + GAP + OFFSET, CLOUD_HEIGHT / 2);
+    ctx.fillText('Вы успешно закончили игру.', CLOUD_POSITION_X + GAP + OFFSET, CLOUD_HEIGHT / 2 + TEXT_HEIGHT);
   } else {
+    /* Выводит победный текст */
+    ctx.fillText('Ура вы победили!', CLOUD_POSITION_X + GAP + OFFSET, CLOUD_POSITION_Y + PADDING_TOP + OFFSET);
+    ctx.fillText('Список результатов:', CLOUD_POSITION_X + GAP + OFFSET, CLOUD_POSITION_Y + PADDING_TOP + OFFSET + TEXT_HEIGHT);
+
     /* Находит максимальное число в массиве */
     var maxTime = getMaxElement(times);
 
@@ -104,17 +113,15 @@ window.renderStatistics = function (ctx, names, times) {
     var transparencyCoefficient = 1 / (names.length - 1);
 
     /* Вызывает сортировку времени прохождения всех игроков и записывает в переменную*/
-    var sortedTimes = sortingPlayersTimes(names, times);
+    var sortedTimes = sortPlayersTimes(names, times);
 
     /* Рисует гистогнамму */
     for (var i = 0; i < names.length; i++) {
       ctx.fillText(names[i], startPositionX + (BAR_WIDTH + BAR_GAP) * i, startPositionY);
 
-      if (names[i] === 'Вы') {
-        ctx.fillStyle = CURRENT_PLAYER_BAR_COLOR;
-      } else {
-        ctx.fillStyle = 'rgba(0, 0, 255,' + (1 - transparencyCoefficient * sortedTimes.indexOf(times[i])).toFixed(2) + ')';
-      }
+      ctx.fillStyle = (names[i] === 'Вы')
+        ? CURRENT_PLAYER_BAR_COLOR
+        : 'rgba(0, 0, 255,' + (1 - transparencyCoefficient * sortedTimes.indexOf(times[i])).toFixed(2) + ')';
 
       var currentBarHeight = MAX_BAR_HEIGHT * times[i] / maxTime;
 
